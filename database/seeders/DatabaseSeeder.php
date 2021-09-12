f8372534df1e33c8b49ef1691122e4ba49c943ed
mdabbas-cse <gmabbas44@gmail.com>
2021-09-12 22:11:32 +0600

    add admin module

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(Admin::class);
+        $this->call(AdminSeeder::class);
     }
 }
